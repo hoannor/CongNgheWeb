69742faf59a18822487d331790a78aa8d5b9641a
hieuwed <130844328+hieuwed@users.noreply.github.com>
2024-11-03 20:35:56 +0700

    update about, contact and policy

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,9 +1,36 @@
 import React from "react";
 import Layout from "./../components/Layout/Layout";
+import {BiBeenHere, BiMailSend, BiPhoneCall, BiSupport} from "react-icons/bi";
 const Contact = () => {
   return (
     <Layout title={"Contact us - Ecommerce app"}>
-      <h1>Contact</h1>
+     <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/contact.jpg"
+            alt="contactus"
+            style={{width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <h1 className="bg-dark p-2 text-white text-center">LIÊN HỆ </h1>
+          <p className="text-justify mt-2">
+          Mọi thắc mắc và thông tin về sản phẩm, vui lòng gọi bất cứ lúc nào chúng tôi sẵn sàng 24/7
+          </p>
+          <p className="mt-3">
+            <BiBeenHere /> : Số 1 Đ. Đại Cồ Việt, Bách Khoa, Hai Bà Trưng, Hà Nội
+          </p>
+          <p className="mt-3">
+            <BiMailSend /> : www.help@commerceapp.com
+          </p>
+          <p className="mt-3">
+            <BiPhoneCall /> : 012 345 6789 
+          </p>
+          <p className="mt-3">
+            <BiSupport/> : 1800-0000
+          </p>
+        </div>
+      </div>
     </Layout>
   );
 };
--- client/src/pages/Policy.js
+++ client/src/pages/Policy.js
@@ -3,8 +3,25 @@ import Layout from "./../components/Layout/Layout";
 
 const Policy = () => {
   return (
-    <Layout title={"Privacy Policy"}>
-      <h1>Policy</h1>
+    <Layout title={"Privacy Policy - Ecommerce app"}>
+      <div className="row policy">
+        <div className="col-md-6">
+          <img
+            src="/images/policy.jpg"
+            alt="privacy policy"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <h1 className="bg-dark p-2 text-white text-center">Chính sách bảo mật</h1>
+          <p className="text-justify mt-2">
+            Chúng tôi cam kết bảo vệ quyền riêng tư của bạn và đảm bảo rằng dữ liệu
+            cá nhân của bạn được bảo mật và chỉ được sử dụng theo cách mà bạn mong đợi. 
+            Chính sách bảo mật này cung cấp chi tiết về cách chúng tôi thu thập, sử dụng, 
+            và bảo vệ thông tin cá nhân của bạn khi sử dụng nền tảng thương mại điện tử của chúng tôi.
+          </p>
+        </div>
+      </div>
     </Layout>
   );
 };

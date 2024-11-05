e895053ea99ed0327356562cc9ebcbd1884c59f6
hoannor <halong4399@gmail.com>
2024-11-05 14:54:36 +0700

    page not found, navbar, footer, about, contact us, policy update

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import Layout from '../components/Layout/Layout'
+import React from "react";
+import Layout from "./../components/Layout/Layout";
 
 const About = () => {
   return (
     <Layout>
-        <h1>About Page</h1>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/about.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p className="text-justify mt-2">
+            something about us
+          </p>
+        </div>
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
+import { BiMailSend, BiPhoneCall, BiSupport } from 'react-icons/bi'
 
 const Contact = () => {
   return (
     <Layout>
-        <h1>Contact Page</h1>
+        <div className='row contactus '>
+          <div className='col-md-6 '>
+            <img src='/images/contactus.jpeg'
+            alt='contactus'
+            style={{ width: "100%" }}
+            />
+          </div>
+          <div className='col-md-4'>
+            <h1 className='bg-dark p-2 text-white text-center'>CONTACT US</h1>
+            <p className='text-justify mt-2'>
+              any query and info about product feel free to call anytime we 24/7 avalable
+            </p>
+            <p className='mt-3'>
+              <BiMailSend /> : halong4399@gmail.com
+            </p>
+            <p className='mt-3'>
+              <BiPhoneCall /> : 0906195855
+            </p>
+            <p className='mt-3'>
+              <BiSupport /> : 0906195855 (Mr. Lucas Nguyen)
+            </p>
+
+          </div>
+        </div>
     </Layout>
   )
 }

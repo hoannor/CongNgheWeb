256a6599a4362cbd4a6257ebe75c2b70d4a4b12f
hoannor <halong4399@gmail.com>
2024-11-06 06:34:20 +0700

    register/login update

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,17 +1,37 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import { Helmet } from 'react-helmet';
+import { Toaster } from 'react-hot-toast';
 
-const Layout = ({children}) => {
+const Layout = ({ children, title, description, keywords, author }) => {
   return (
     <div>
+        <Helmet>
+                <meta charSet="utf-8" />
+                
+                <meta name="description" content={description}/>
+                <meta name="keywords" content={keywords}/>
+                <meta name="author" content={author}/>
+
+                <title>{title}</title>
+        </Helmet>
+
         <Header />
         <main style={{minHeight: "70vh"}}>
+            <Toaster />
             {children}
         </main>
         <Footer />
     </div>
   )
+}
+
+Layout.defaultProps = {
+  title: 'Web ban hang SPA - Shoppee fake',
+  description: 'Our Web Project',
+  keywords: 'mern, react, node, mongodb',
+  author: 'Our Web group',
 }
 
 export default Layout
--- controllers/authController.js
+++ controllers/authController.js
@@ -7,26 +7,26 @@ export const registerController = async (req, res) => {
         const {name, email, password, phone, address} = req.body
         // validation
         if(!name){
-            return res.send({error: 'Name is required'})
+            return res.send({message: 'Name is required'})
         }
         if(!email){
-            return res.send({error: 'Email is required'})
+            return res.send({message: 'Email is required'})
         }
         if(!password){
-            return res.send({error: 'Password is required'})
+            return res.send({message: 'Password is required'})
         }
         if(!phone){
-            return res.send({error: 'Phone number is required'})
+            return res.send({message: 'Phone number is required'})
         }
         if(!address){
-            return res.send({error: 'Address is required'})
+            return res.send({message: 'Address is required'})
         }
         // check user
         const exisitingUser = await userModel.findOne({email})
         // exisiting user
         if(exisitingUser){
             return res.status(200).send({
-                success: true,
+                success: false,
                 message: 'Already Resgiser please login',
             })
         }

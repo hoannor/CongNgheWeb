a97f018cb18ba00bd39e49f370909b8d0adf6ec1
hoannor <halong4399@gmail.com>
2024-11-09 05:27:30 +0700

    Forgot Password and Reset Password

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -11,13 +11,14 @@ const Register = () => {
     const [password, setPassword] = useState("")
     const [phone, setPhone] = useState("")
     const [address, setAddress] = useState("")
+    const [answer, setAnswer] = useState("")
     const navigate = useNavigate()
 
     // form function
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post(`/api/v1/auth/register`, {name, email, password, phone, address});
+            const res = await axios.post(`/api/v1/auth/register`, {name, email, password, phone, address, answer});
             if(res && res.data.success){
                 toast.success(res.data && res.data.message);
                 navigate('/login');
@@ -52,6 +53,10 @@ const Register = () => {
                 
                 <div className="mb-3">
                     <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} className="form-control" id="exampleInputAddress" placeholder='Enter Your Address' required/>
+                </div>
+
+                <div className="mb-3">
+                    <input type="text" value={answer} onChange={(e) => setAnswer(e.target.value)} className="form-control" id="exampleInputAnswer" placeholder='What Is Your Mother Name' required/>
                 </div>
 
                 <button type="submit" className="btn btn-primary">REGISTER</button>

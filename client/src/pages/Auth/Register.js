2155599cf21b0a011e1684ac6b39b6210124a2e5
BCHoang <buihoang3002@gmail.com>
2024-11-09 19:27:46 +0700

    update login/register 9/11

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -9,7 +9,7 @@ const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [phone, setPhone] = useState("");
+  const [phone, setPhone] = useState("")
   const [address, setAddress] = useState("");
   const navigate = useNavigate();
 

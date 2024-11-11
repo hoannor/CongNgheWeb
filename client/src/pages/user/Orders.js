f58063cd0de5eff5f2c6ac7b2efa1e37d4a481f9
hoannor <halong4399@gmail.com>
2024-11-12 01:26:28 +0700

    admin panel and user panel BE and FE

--- client/src/pages/user/Orders.js
+++ client/src/pages/user/Orders.js
@@ -1,28 +1,23 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
 import UserMenu from '../../components/Layout/UserMenu'
-import { useAuth } from '../../context/auth'
 
-const Dashboard = () => {
-  const [auth] = useAuth()
+const Orders = () => {
   return (
-    <Layout title={"Dashboard"}>
+    <Layout title={"Dashboard - Orders"}>
         <div className='container-fluid p-3 m-3'>
             <div className='row'>
                 <div className='col-md-3'>
                     <UserMenu />
                 </div>
                 <div className='col-md-9'>
-                  <div className='card w-75 p-3'> 
-                    <h3>{auth?.user?.name}</h3>
-                    <h3>{auth?.user?.email}</h3>
-                    <h3>{auth?.user?.address}</h3>
-                  </div>
+                    <h1>All Orders</h1>
                 </div>
             </div>
         </div>
+        
     </Layout>
   )
 }
 
-export default Dashboard
+export default Orders

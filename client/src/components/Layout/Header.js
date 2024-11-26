87b936fa69dad42b6f4e19413fff2864655390a7
hieuwed <130844328+hieuwed@users.noreply.github.com>
2024-11-26 23:19:11 +0700

    update category page

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -4,9 +4,11 @@ import { FaBagShopping } from "react-icons/fa6";
 import { useAuth } from '../../context/auth';
 import toast from 'react-hot-toast';
 import SearchInput from '../Form/SearchInput';
+import useCategory from '../../hooks/useCategory';
 
 const Header = () => {
   const [auth, setAuth] = useAuth();
+  const categories = useCategory();
   const handleLogout = () => {
     setAuth({
       ...auth,
@@ -30,9 +32,26 @@ const Header = () => {
                 <li className="nav-item">
                   <NavLink to="/" className="nav-link">Home</NavLink>
                 </li>
-                <li className="nav-item">
-                  <NavLink to="/category" className="nav-link">Category</NavLink>
+                <li className="nav-item dropdown">
+                  <Link className="nav-link dropdown-toggle" 
+                  to={"/categories"} 
+                  data-bs-toggle="dropdown" 
+                  >
+                    Categories
+                  </Link>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <Link className="dropdown-item" to={"/categories"}>
+                         All Categories
+                      </Link>
+                    </li>
+                    {categories?.map((c) => (
+                      <li>
+                        <Link className="dropdown-item" to={`/category/${c.slug}`} >{c.name}</Link></li>
+                  ))}
+                  </ul>
                 </li>
+
                 {
                   !auth.user ? (
                   <>

b13cdb93e06dada74160e454c1ab9a1c428810f7
hoannor <halong4399@gmail.com>
2024-11-29 00:45:07 +0700

    cart page update

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -4,10 +4,12 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { Checkbox, Radio } from "antd";
 import { Prices } from "../components/Prices";
-import { get, set } from "mongoose";
+import { useCart } from "../context/cart";
+import toast from "react-hot-toast";
 
 const HomePage = () => {
   const navigate = useNavigate()
+  const [cart, setCart] = useCart() 
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
   const [checked, setChecked] = useState([]);
@@ -165,7 +167,11 @@ const HomePage = () => {
                     <p className="card-text"> $ {p.price}</p>
                     <button class="btn btn-primary ms-1 mb-1" 
                     onClick={() => navigate(`/product/${p.slug}`)}>More Details</button>
-                    <button class="btn btn-secondary ms-1">ADD TO CART</button>
+                    <button class="btn btn-secondary ms-1" onClick={() => {
+                      setCart([...cart, p])
+                      localStorage.setItem('cart', JSON.stringify([...cart, p]))
+                      toast.success("Item added to cart")
+                    }}>ADD TO CART</button>
                   </div>
                 </div>
               ))}
